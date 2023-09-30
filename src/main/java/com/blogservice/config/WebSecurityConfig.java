a97d8082b74465b2df7df0b0b67a6129d780f6cf
KangJekoo <peter6998@naver.com>
2023-09-30 17:31:36 +0900

    Fix: Must use Lambda function with Spring Security Version over 6.1.0

--- src/main/java/com/blogservice/config/WebSecurityConfig.java
+++ src/main/java/com/blogservice/config/WebSecurityConfig.java
@@ -7,11 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
     private final UserDetailService userService;
@@ -23,22 +25,28 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain finterChain(HttpSecurity http) throws Exception{
-        return http
-                .authorizeRequests()
-                .requestMatchers(("/login", "/signup", "/user").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .loginPage("/login")
-                .defaultSuccessUrl("/articles")
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login")
-                .invalidateHttpSession(true)
-                .and()
-                .csrf().disable()
-                .build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/login", "/signup", "/user").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/articles")
+                )
+                .logout(logout ->
+                        logout
+                                .logoutSuccessUrl("/login")
+                                .invalidateHttpSession(true)
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http,
